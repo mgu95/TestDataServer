c9e37d0e85fe237cdfb7420ff3c02d8a9f6b183d
mgu95 <mariuszgusc@outlook.com>
2023-03-04 22:23:43 +0100

    Initial commit

---
+++ src/main/java/pl/mgu95/testDataServer/TestDataServerApplication.java
@@ -0,0 +1,12 @@
+package pl.mgu95.testDataServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDataServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestDataServerApplication.class, args);
+    }
+}
---
+++ src/main/java/pl/mgu95/testDataServer/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package pl.mgu95.testDataServer.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String getCity() {
+        return "Hello from TestDataServer :)";
+    }
+}
